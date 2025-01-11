6b2760de951d3dc3a485c7da373ae761817fee63
Akash Nagar <akashnagar47@outlook.com>
2025-01-12 04:27:57 +0530

    added ServiceDefaults reference

--- src/Products/Program.cs
+++ src/Products/Program.cs
@@ -6,8 +6,10 @@ using Products.Endpoints;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.AddServiceDefaults();
+
 //Add Keyvault client
 builder.AddAzureKeyVaultClient("secrets", settings => settings.DisableHealthChecks = true);
+
 builder.Services.AddDbContext<ProductDataContext>(options =>
     options.UseSqlite(builder.Configuration.GetConnectionString("ProductsContext") ?? throw new InvalidOperationException("Connection string 'ProductsContext' not found.")));
 
