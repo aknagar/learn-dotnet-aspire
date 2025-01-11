2936570987d6ce87d83a5617a456a012db782e17
Akash Nagar <akashnagar47@outlook.com>
2025-01-11 20:44:00 +0530

    added aspire projects

---
+++ eShopLite.AppHost/Program.cs
@@ -0,0 +1,10 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var products = builder.AddProject<Projects.Products>("products"); //products name will be used to discover the address of the Products project.
+
+builder.AddProject<Projects.Store>("store")
+    .WithExternalHttpEndpoints()
+    .WithReference(products);  // store project depends on products project
+
+
+builder.Build().Run();
