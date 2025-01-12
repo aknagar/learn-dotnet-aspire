86a2d2dbfa414be8489acf52394bf8ab49298957
Akash Nagar <akashnagar47@outlook.com>
2025-01-12 21:53:55 +0530

    added keyvault integration

--- src/eShopLite.AppHost/Program.cs
+++ src/eShopLite.AppHost/Program.cs
@@ -1,14 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var products = builder.AddProject<Projects.Products>("products"); //products name will be used to discover the address of the Products project.
+/*
+var secrets = builder.ExecutionContext.IsPublishMode
+    ? builder.AddAzureKeyVault("secrets")
+    : builder.AddConnectionString("secrets");
+*/
+var secrets = builder.AddConnectionString("secrets");
+
+var products = builder.AddProject<Projects.Products>("products").WithReference(secrets); //products name will be used to discover the address of the Products project.
 
 builder.AddProject<Projects.Store>("store")
     .WithExternalHttpEndpoints()
     .WithReference(products);  // store project depends on products project
 
-
-builder.AddProject<Projects.eShopLite_Api>("eshoplite-api");
-
+builder.AddProject<Projects.eShopLite_Api>("eshoplite-api")
+    .WithReference(secrets);
+    
 
 builder.AddProject<Projects.eShopLite_Worker>("eshoplite-worker");
 
